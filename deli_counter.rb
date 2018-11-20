88afffc9c89b645d647b90e7ab3b36c3cd30e566
Anthony Gregg <agregg22@gmail.com>
2018-11-20 19:43:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
-katz_deli = []
+katz_deli = [55, 49, 70]
 other_deli = ["Logan", "Avi", "Spencer"] 
 another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] 
 
 
 def line(deli)
-  if deli.count > 1
+  if deli.count > 0
     output = "The line is currently:"
     deli.each_with_index do |name, index|
       output << " #{index +1}. #{name}"
@@ -15,16 +15,25 @@ def line(deli)
   end
 end
 
-def take_a_number(deli,name)
-  deli.push("#{name}")
- puts "Welcome, #{deli[-1]}. You are number #{deli.length} in line."
+last_number_out = 0
+
+def take_a_number(deli)
+ new_customer = last_number_out + 1
+  deli.push(new_customer)
+  puts "Hi ticket number #{deli[-1]}, there are #{deli.count} people in line."
+  last_number_out = new_customer
 end
 
 def now_serving(deli)
-  if deli.count > 1
+  if deli.count > 0
     output = "Currently serving #{deli.shift}."
     puts output
   else
     puts "There is nobody waiting to be served!"
   end
 end
+
+
+def ticket_machine(deli, index)
+  deli.push(index)
+  puts 
